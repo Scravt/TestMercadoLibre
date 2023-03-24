e651034acc875c14be03240aaede3a2c5f6b7873
Gonzalo <34680110ax@gmail.com>
2023-03-24 14:00:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { CajaBusqueda } from './componentes/CajaBusqueda'
+
+function App() {
+  return (
+    <div>
+      <CajaBusqueda/>
+    </div>
+  )
+}
+
+export default App
